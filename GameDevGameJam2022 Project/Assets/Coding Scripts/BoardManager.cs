73a90adf9ff4245f6c477c993da207545fad2c8d
joshuahoem <joshuahoem@gmail.com>
2022-05-24 21:06:51 -0700

    New Pieces and updated movement

--- GameDevGameJam2022 Project/Assets/Coding Scripts/BoardManager.cs
+++ GameDevGameJam2022 Project/Assets/Coding Scripts/BoardManager.cs
@@ -5,15 +5,20 @@ using CodeMonkey.Utils; //make own code so you can delete this
 
 public class BoardManager : MonoBehaviour
 {
-    public GridMaker<int> gridMaker;
     [SerializeField] int width = 5;
     [SerializeField] int height = 5;
     [SerializeField] float cellSize = 10f;
-    [SerializeField] Vector3 originPoint = new Vector3(0,0);
+    [SerializeField] Vector3 originPosition = new Vector3(0,0);
 
-    void Start()
+    //States
+    public bool selected = false;
+
+    //Cache
+    public GridMaker<int> gridMaker;
+
+    void Awake()
     {
-        gridMaker = new GridMaker<int>(width, height, cellSize, originPoint);
+        gridMaker = new GridMaker<int>(width, height, cellSize, originPosition);
     }
 
     private void Update() {
@@ -27,6 +32,20 @@ public class BoardManager : MonoBehaviour
         {
             Debug.Log(gridMaker.GetValue(UtilsClass.GetMouseWorldPosition()));
         }
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        Debug.Log(gridMaker + " " + this.name);
+        gridMaker.SetValue(x, y, value);
     }
     
 } 
--- GameDevGameJam2022 Project/Assets/Coding Scripts/NecroMan.cs
+++ GameDevGameJam2022 Project/Assets/Coding Scripts/NecroMan.cs
@@ -5,18 +5,32 @@ using CodeMonkey.Utils; //make own code so you can delete this
 
 public class NecroMan : MonoBehaviour
 {
+    //Numbers
     [SerializeField] float minSpeed = 10f;
     [SerializeField] int moveDistance = 3;
+    [SerializeField] int pieceValue = 1;
+    [SerializeField] int sizeClass = 5;
+    [SerializeField] int attackRange = 1;
+
+    //States
     [SerializeField] public bool selected = false;
     [SerializeField] public bool moved = true;
+    [SerializeField] bool canMove = false;
+    [SerializeField] bool canRegenerate = true;
+    [SerializeField] bool canBeAttacked = true;
+    [SerializeField] bool displaySize = true;
+
+    //Cache
     [SerializeField] GameObject moveOutline;
     [SerializeField] private Vector3 targetPosition;
     private Vector3 currentPosition;
     List<GameObject> moveTiles = new List<GameObject>();
+    enum Team {Player, Enemy, Neutral}
+    [SerializeField] Team team;
 
     MouseControl mouseControl;
     BoardManager boardManager;
-    GridMaker<int> grid;
+    static GridMaker<int> grid;
     
     private void Start() {
         mouseControl = FindObjectOfType<MouseControl>();
@@ -24,7 +38,9 @@ public class NecroMan : MonoBehaviour
         grid = boardManager.gridMaker;
         currentPosition = RoundVector(transform.position);
         ShowMoves(false);
-        grid.SetValue(currentPosition, 1); // 1 for necromancer
+
+        //Debug.Log(boardManager.gridMaker + " " + this.name);
+        grid.SetValue(currentPosition, pieceValue); // 1 for necromancer
         //Debug.Log(grid.GetX(currentPosition)+ " " + grid.GetY(currentPosition));
         targetPosition = currentPosition;
         moved = false;
@@ -37,7 +53,9 @@ public class NecroMan : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (!selected) {return;}
+            
+            if (!canMove) { return; }
+            if (!selected) { return; }
 
             
             targetPosition = RoundVector(UtilsClass.GetMouseWorldPosition());
@@ -53,18 +71,33 @@ public class NecroMan : MonoBehaviour
             x = Mathf.FloorToInt(currentPosition.x);
             y = Mathf.FloorToInt(currentPosition.y);
             if (Mathf.Abs(grid.GetX(targetPosition)-x) > moveDistance || Mathf.Abs(grid.GetY(targetPosition)-y) > moveDistance) {return;}
-            if (grid.GetValue(UtilsClass.GetMouseWorldPosition()) == 0) //0 is open so move there
+            int positionValue = grid.GetValue(UtilsClass.GetMouseWorldPosition());
+            if (positionValue == 0) //0 is open so move there
             {
                 //move
-                grid.SetValue(UtilsClass.GetMouseWorldPosition(),1);            //1 for your Necromancer
-                Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+                grid.SetValue(UtilsClass.GetMouseWorldPosition(),pieceValue);            //1 for your Necromancer
+               //Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
 
                 //time to move
                 grid.SetValue(currentPosition, 0); // set old space as available
                 ShowMoves(false);
                 moved = false;
                 selected = false;
+                boardManager.selected = false;
                 mouseControl.hoverSquareEnabled = false; //set to false after move, but dont follow mouse anymore
+            }
+            else if (positionValue != 0 && positionValue != 100) //100 is obstacles
+            {
+                //check to see if you can attack
+                if (Mathf.Abs(grid.GetX(targetPosition)-x) <= attackRange && Mathf.Abs(grid.GetY(targetPosition)-y) <= attackRange)
+                {
+                    Debug.Log("Attack");
+                    //identify piece
+                    //identify team
+                    //deal damage
+                    //display size and update after damage
+                    //identify if it can be attacked
+                }
             }
 
         }
@@ -74,11 +107,14 @@ public class NecroMan : MonoBehaviour
     private void OnMouseDown()
     {
         //select piece
+        if (boardManager.selected) {return;}
+        if (!canMove) { return; }
         selected = !selected;
+        boardManager.selected = selected;
 
         mouseControl.hoverSquareEnabled = selected;
 
-        ShowMoves(true);
+        ShowMoves(selected);
 
         //check if it is your piece or not/ what is there
         
@@ -123,6 +159,7 @@ public class NecroMan : MonoBehaviour
             {
                 Destroy(tile.gameObject);
             }
+            moveTiles.Clear();
         }
         else
         {
@@ -130,8 +167,10 @@ public class NecroMan : MonoBehaviour
             {
                 for (int y = (int) (currentPosition.y - moveDistance); y <= (int) (currentPosition.y + moveDistance); y++)
                 {
+                    if (grid.GetValue(x,y) != 0) {continue;}
                     if (grid.InBounds(x,y))
                     {
+                        
                         GameObject tile = Instantiate(moveOutline, new Vector3 (x + 0.5f,y + 0.5f), Quaternion.identity);
                         moveTiles.Add(tile);
                     }
